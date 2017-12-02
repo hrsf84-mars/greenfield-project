7a47ca12dcab62388bd0cbe601428edc93424ce8
Tim Trautman <tctrautman@gmail.com>
2017-12-01 16:17:48 -0800

    Fix styling with confirm team button

--- client/src/components/TeamList.jsx
+++ client/src/components/TeamList.jsx
@@ -6,15 +6,17 @@ const TeamList = (props) => {
     <div className={css.listPokemon}>
       <h3 >Selected Team</h3>
       <br />
-      {props.pokemon.map((pokemon) => {
-        return (
-          <div style={{ display: 'inline-block', width: '150px' }} key={pokemon.name}>
-            <img src={pokemon.sprites.front_default} alt="" />
-            <h5 style={{ marginBottom: '0px', marginTop: '2px' }}>{pokemon.name}</h5>
-            <h6 style={{ marginBottom: '0px' }}>{pokemon.health} / {pokemon.initialHealth}</h6>
-          </div>
-        );
-      })}
+      <div className={css.teamList} >
+        {props.pokemon.map((pokemon) => {
+          return (
+            <div style={{ display: 'inline-block', width: '150px' }} key={pokemon.name}>
+              <img src={pokemon.sprites.front_default} alt="" />
+              <h5 style={{ marginBottom: '0px', marginTop: '2px' }}>{pokemon.name}</h5>
+              <h6 style={{ marginBottom: '0px' }}>{pokemon.health} / {pokemon.initialHealth}</h6>
+            </div>
+          );
+        })}
+      </div>
       <br />
       <br />
       <button style={{ display: 'block' }} onClick={props.handleConfirm} className={css.gameButton}>
